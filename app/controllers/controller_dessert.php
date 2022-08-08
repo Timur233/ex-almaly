d797e386124eda9608ee9de8de989926986119d3
Iskandarov Timur <timur.iskandarov96@gmail.com>
2022-08-08 16:03:23 +0600

    first commit

---
+++ app/controllers/controller_dessert.php
@@ -0,0 +1,26 @@
+<?php
+    namespace app\controllers;
+
+    use app\core\Controller;
+    use app\models\Model_Building_Steps;
+    use app\models\Model_Main;
+    use app\core\View;
+
+    include "app/models/model_main.php";
+    include "app/models/model_building_steps.php";
+
+    class Controller_Dessert extends Controller
+    {
+        function action_index() {
+            $data = Model_Main::get_data(LANG);	
+            $data['footer']	= Model_Main::get_footer(LANG);
+
+            if (PROJECT_ID !== ''):
+                $data['building_steps'] = Model_Building_Steps::get_steps(LANG);
+            endif;
+
+            View::generate('dessert.twig', $data);
+        }
+        
+    }
+?>
